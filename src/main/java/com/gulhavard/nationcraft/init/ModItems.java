06f5c32b208090ec95149dfac34ba0b8a8580ff9
PhantomNiqht <32378632+PhantomNiqht@users.noreply.github.com>
2018-05-05 00:59:12 -0700

    Built Barebones Handlers
    The Mod will now load into Forge upon MineCraft launch...

---
+++ src/main/java/com/gulhavard/nationcraft/init/ModItems.java
@@ -0,0 +1,9 @@
+package com.gulhavard.nationcraft.init;
+
+public class ModItems {
+	
+	//public static final List<Item> ITEMS = new ArrayList<Item>();
+	
+	//public static final Item COPPER_INGOT = new ItemBase("copper_ingot");
+
+}
--- src/main/java/com/gulhavard/nationcraft/util/Reference.java
+++ src/main/java/com/gulhavard/nationcraft/util/Reference.java
@@ -6,6 +6,7 @@ public class Reference {
 	public static final String NAME = "NationCraft";
 	public static final String VERSION = "0.1.0-alpha";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String CLIENT_PROXY_CLASS = "com.gulhavard.nationcraft.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.gulhavard.nationcraft.proxy.CommonProxy";
 	
 }
