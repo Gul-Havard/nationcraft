325a699dfaf6f9f4231b2c2fb917d4f73e8c6eec
PhantomNiqht <32378632+PhantomNiqht@users.noreply.github.com>
2018-05-11 22:56:53 -0700

    Corrected World Gen and Models

--- src/main/java/com/gulhavard/nationcraft/init/ModBlocks.java
+++ src/main/java/com/gulhavard/nationcraft/init/ModBlocks.java
@@ -3,8 +3,11 @@ package com.gulhavard.nationcraft.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.gulhavard.nationcraft.blocks.BlockAluminumOre;
 import com.gulhavard.nationcraft.blocks.BlockBase;
-import com.gulhavard.nationcraft.blocks.BlockOre;
+import com.gulhavard.nationcraft.blocks.BlockCopperOre;
+import com.gulhavard.nationcraft.blocks.BlockTinOre;
+import com.gulhavard.nationcraft.blocks.BlockZincOre;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -13,10 +16,10 @@ public class ModBlocks {
 	
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
-	public static final Block COPPER_ORE = new BlockOre("copper_ore", "overworld");
-	public static final Block TIN_ORE = new BlockOre("tin_ore", "overworld");
-	public static final Block ZINC_ORE = new BlockOre("zinc_ore", "overworld");
-	public static final Block ALUMINUM_ORE = new BlockOre("aluminum_ore", "overworld");
+	public static final Block COPPER_ORE = new BlockCopperOre("copper_ore", "overworld");
+	public static final Block TIN_ORE = new BlockTinOre("tin_ore", "overworld");
+	public static final Block ZINC_ORE = new BlockZincOre("zinc_ore", "overworld");
+	public static final Block ALUMINUM_ORE = new BlockAluminumOre("aluminum_ore", "overworld");
 	public static final Block COPPER_BLOCK = new BlockBase("copper_block", Material.IRON);
 
 }
